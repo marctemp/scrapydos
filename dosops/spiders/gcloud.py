75ca859c6ed261b2642c4076094266193463ad2c
catherineed <56081396+catherineed@users.noreply.github.com>
2020-10-15 14:52:30 +0100

    gcloud services spider

--- dosops/spiders/gcloud.py
+++ dosops/spiders/gcloud.py
@@ -2,26 +2,25 @@ import scrapy
 
 class GcloudSpider(scrapy.Spider):
     name = "gcloud"
+    
 
     def start_requests(self):
-        baseurl = 'https://www.digitalmarketplace.service.gov.uk/g-cloud/search?page='
-        for x in range(1,50):
-            yield scrapy.Request(url=baseurl + str(x) + '&q=data+analytics', callback=self.parse)
+        baseurl = 'https://www.digitalmarketplace.service.gov.uk/g-cloud/services/624695628011497'
+        yield scrapy.Request(url=baseurl, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-1]
         self.logger.warning(response.url)
-        filename = 'gcloud.csv'
-        for section in response.selector.xpath('//div[@class="search-result"]'):
-            company = section.xpath('p[1]/text()').extract_first()
-            offering = section.xpath('h2/a/text()').extract_first()
-            link = 'https://www.digitalmarketplace.service.gov.uk' + section.xpath('h2/a/@href').extract_first()
-            description = section.xpath('p[2]/text()').extract_first()
-            category = section.xpath('ul/li[1]/text()').extract_first()
-            gcloud = section.xpath('ul/li[2]/text()').extract_first()
-            entry = company.strip().replace('\n', ' ').replace('\r', '') + '|' + offering.strip().replace('\n', ' ').replace('\r', '') + '|' + description.strip().replace(
-                '\n', ' ').replace('\r', '').replace('|', '') + '|' + category.strip().replace('\n', ' ').replace('\r', '') + '|' + gcloud.strip().replace('\n', ' ').replace('\r', '') + '|' + link.strip().replace('\n', ' ').replace('\r', '') + '\n'
-            self.logger.warning(entry)            
+        entry = ''
+        filename = 'gcloud-services.csv'
+        count = 1
+        countstr = str(count)
+        countfill = countstr.zfill(2)
+        while countfill < str(22):
+            field = response.xpath('//div[contains(@id,"'+ countfill + '")]//div[1]//text()')
+            field_text = response.xpath('//div[contains(@id,"01")]//div[1]//dd//text()')
+            count += 1
+            entry += field + '\n' + field_text
+            self.logger.warning(entry)
             with open(filename, 'ab') as f:
                 f.write(entry.encode('utf-8'))
-        self.log('Saved file %s' % filename)
+            self.log('Saved file %s' % filename)
