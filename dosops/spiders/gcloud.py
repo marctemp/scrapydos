2d63c65a2f79da1408bbb336af726ebe3d084ba0
catherineed <56081396+catherineed@users.noreply.github.com>
2020-10-20 15:19:40 +0100

    gcloud scraper working

--- dosops/spiders/gcloud.py
+++ dosops/spiders/gcloud.py
@@ -1,14 +1,19 @@
 import scrapy
 from json import dump
-#from Utilities import isValidXpath
+from tqdm import tqdm
+from dosops.spiders.Utilities import is_valid_xpath, load_ids
 
 
 class GcloudSpider(scrapy.Spider):
     name = "gcloud"
 
-    def start_requests(self):
-        baseurl = 'https://www.digitalmarketplace.service.gov.uk/g-cloud/services/624695628011497'
-        yield scrapy.Request(url=baseurl, callback=self.parse)
+    def start_requests(self, ids=None):
+        if not ids:
+            baseurl = 'https://www.digitalmarketplace.service.gov.uk/g-cloud/services/'
+            ids = load_ids(
+                '/Users/catherined/Desktop/procurment_analysis/gcloud-IDs.csv', 'ID')
+        for id in tqdm(ids):
+            yield scrapy.Request(url=baseurl + str(id), callback=self.parse)
 
     def parse(self, response):
         self.logger.warning(response.url)
@@ -16,32 +21,32 @@ class GcloudSpider(scrapy.Spider):
         entry = {}
         filename = 'gcloud-services.json'
 
-        countSection = 1
-        countDiv = 1
+        count_section = 1
+        count_div = 1
         is_section = True
         while is_section:
-            count_formatted = str(countSection).zfill(2)
+            count_formatted = str(count_section).zfill(2)
             is_div = True
             try:
-                # isValidXpath(response.xpath(
-                #  f'//div[contains(@id,"{count_formatted}")]'))
+                is_valid_xpath(response.xpath(
+                    f'//div[contains(@id,"{count_formatted}")]'))
                 while is_div:
                     try:
                         fields = response.xpath(
-                            f'//div[contains(@id,"{count_formatted}")]//div[{countDiv}]//dt//text()')[0].root.strip()
+                            f'//div[contains(@id,"{count_formatted}")]//div[{count_div}]//dt//text()')[0].root.strip()
                         fields_text = response.xpath(
-                            f'//div[contains(@id,"{count_formatted}")]//div[{countDiv}]//dd//text()')[0].root.strip()
+                            f'//div[contains(@id,"{count_formatted}")]//div[{count_div}]//dd//text()')[0].root.strip()
 
                         entry[fields] = fields_text
                         self.logger.warning(
                             f'{list(entry.keys())[-1]}: {list(entry.values())[-1]}')
 
-                        countDiv += 1
+                        count_div += 1
                     except:
                         is_div = False
-                        print(f'No div numbered {countDiv}')
-                countSection += 1
-                countDiv = 1
+                        print(f'No div numbered {count_div}')
+                count_section += 1
+                count_div = 1
             except:
                 is_section = False
                 print(f'No Section numbered {count_formatted}')
