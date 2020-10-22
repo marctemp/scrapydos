2a772df909516320fbacd59d5020ce0ef03daa72
catherineed <56081396+catherineed@users.noreply.github.com>
2020-10-22 14:30:08 +0100

    scraper update

--- dosops/spiders/gcloud.py
+++ dosops/spiders/gcloud.py
@@ -8,12 +8,26 @@ class GcloudSpider(scrapy.Spider):
     name = "gcloud"
 
     def start_requests(self, ids=None):
-        if not ids:
-            baseurl = 'https://www.digitalmarketplace.service.gov.uk/g-cloud/services/'
-            ids = load_ids(
-                '/Users/catherined/Desktop/procurment_analysis/gcloud-IDs.csv', 'ID')
-        for id in tqdm(ids):
-            yield scrapy.Request(url=baseurl + str(id), callback=self.parse)
+       # if not ids:
+        baseurl = 'https://www.digitalmarketplace.service.gov.uk/g-cloud/services/624695628011497'
+       # ids = load_ids(
+        # '/Users/catherined/Desktop/procurment_analysis/gcloud-IDs.csv', 'ID')
+    # for id in tqdm(ids):
+        yield scrapy.Request(url=baseurl, callback=self.parse)
+
+    def get_summary(self, response):
+        supplier = response.xpath(
+            '//span[@class="govuk-caption-l"]//text()')[0].root.strip()
+        project_name = response.xpath(
+            '//h1[@class="govuk-heading-l"]//text()')[0].root.strip()
+        service_summary = response.xpath(
+            '//p[@class="service-summary-lede"]//text()')[0].root.strip()
+
+        right_side = response.xpath(
+            '//div[@id="meta"]//h2//text()' + '//div[@id="meta"]//p//text()')
+
+        features_and_benefits = response.xpath(
+            '//ul[@class="service-summary-features-and-benefits"]//text()')
 
     def parse(self, response):
         self.logger.warning(response.url)
@@ -30,6 +44,7 @@ class GcloudSpider(scrapy.Spider):
             try:
                 is_valid_xpath(response.xpath(
                     f'//div[contains(@id,"{count_formatted}")]'))
+
                 while is_div:
                     try:
                         fields = response.xpath(
@@ -38,6 +53,7 @@ class GcloudSpider(scrapy.Spider):
                             f'//div[contains(@id,"{count_formatted}")]//div[{count_div}]//dd//text()')[0].root.strip()
 
                         entry[fields] = fields_text
+
                         self.logger.warning(
                             f'{list(entry.keys())[-1]}: {list(entry.values())[-1]}')
 
